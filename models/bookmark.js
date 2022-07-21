7480aaa28a2dfa7166801c1d46674bf6497454fc
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-07-21 12:08:54 -0400

    update

--- models/bookmark.js
+++ models/bookmark.js
@@ -4,17 +4,20 @@ module.exports = [
     url: "https://developer.mozilla.org/en-US/",
     isFavorite: true,
     category: "educational",
+    description: "The Mozilla Developer Network",
   },
   {
     name: "Apartment Therapy",
     url: "https://www.apartmenttherapy.com",
     isFavorite: true,
     category: "inspirational",
+    description: "A website for people who want to live in a home",
   },
   {
     name: "DMV",
     url: "https://dmv.ny.gov",
     isFavorite: false,
     category: "adulting",
+    description: "The DMV",
   },
 ];
